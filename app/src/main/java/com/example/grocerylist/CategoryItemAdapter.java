59128b66e2572cd6b10d09cf86e65fd59e2c0e01
TVoNguyen18 <43914530+TVoNguyen18@users.noreply.github.com>
2020-06-13 15:04:15 -0700

    :((((

---
+++ app/src/main/java/com/example/grocerylist/CategoryItemAdapter.java
@@ -0,0 +1,31 @@
+package com.example.grocerylist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CategoryItemAdapter extends ArrayAdapter<CategoryItem> {
+    public CategoryItemAdapter(Context context, ArrayList list) {
+        super(context, 0, list);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.existing_list_item, parent, false);
+        CategoryItem currentCategoryItem = getItem(position);
+        TextView categoryItemTextView = convertView.findViewById(R.id.item_in_grocery_list);
+        categoryItemTextView.setText(currentCategoryItem.getName());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/grocerylist/ListCreationActivity.java
+++ app/src/main/java/com/example/grocerylist/ListCreationActivity.java
@@ -2,13 +2,49 @@ package com.example.grocerylist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListCreationActivity extends AppCompatActivity {
+
+    EditText itemName;
+    String actualItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_creation);
+
+
+        itemName = findViewById(R.id.item_name);
+        actualItem = itemName.getText().toString();
+
+        ListView groceryListView = findViewById(R.id.groceryLV);
+
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+
+        CategoryItemAdapter arrayAdapter = new CategoryItemAdapter
+                (this, stringArrayList);
+
+        groceryListView.setAdapter(arrayAdapter);
+    }
+
+    public void addToList(View view) {
+        SharedPreferences sharedPrefs
+                = getSharedPreferences("MySharedPrefs", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+
+        editor.putString("grocery list", actualItem);
+
+        editor.apply();
+
+        //CategoryItemAdapter iA = new CategoryItemAdapter(this, actualItem);
     }
 }
