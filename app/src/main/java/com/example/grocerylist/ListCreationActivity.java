9b3cb57980c1f873b5b55871ace20f83b2e38c1d
TVoNguyen18 <43914530+TVoNguyen18@users.noreply.github.com>
2020-06-20 14:50:27 -0700

    I think we have something working

--- app/src/main/java/com/example/grocerylist/ListCreationActivity.java
+++ app/src/main/java/com/example/grocerylist/ListCreationActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -31,7 +32,10 @@ public class ListCreationActivity extends AppCompatActivity {
 
     String sharedPrefString = "";
 
-    ArrayList<String> arrayListOfGroceryItems = new ArrayList<String>();
+    CategoryItemAdapter arrayAdapter;
+
+    //ArrayList<String> arrayListOfGroceryItems = new ArrayList<String>();
+    ArrayList<String> dropdown = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,25 +55,37 @@ public class ListCreationActivity extends AppCompatActivity {
 
 
         if (sharedPrefString != "") {
-            sharedPrefString = sharedPrefs.getString(actualListName, "Empty List");
+            sharedPrefString = sharedPrefs.getString("grocery list", "Empty List");
         }
 
-        //sharedPrefString = sharedPrefs.getString("grocery list", "Empty List");
+        sharedPrefString = sharedPrefs.getString("grocery list", "Empty List");
 
         ListView groceryListView = findViewById(R.id.groceryLV);
 
         String[] stringArrayList = sharedPrefString.split(",");
 
-        CategoryItemAdapter arrayAdapter = new CategoryItemAdapter
+        arrayAdapter = new CategoryItemAdapter
                 (this, stringArrayList);
 
         groceryListView.setAdapter(arrayAdapter);
         Toast.makeText(getApplicationContext(),"To put multiple items spearate by putting a comma", Toast.LENGTH_SHORT).show();
 
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        String[] tmpDropdown = dropdown.toArray(new String[dropdown.size()]);
+        //String[] tmpDropdown = {"recommended by the creators", "troll items"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, dropdown);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 
     public void createList(View view) {
         actualListName = listName.getText().toString();
+
+        dropdown.add(actualListName);
 
 //        sharedPrefs
 //                = getSharedPreferences("MySharedPrefs", MODE_PRIVATE);
@@ -83,13 +99,15 @@ public class ListCreationActivity extends AppCompatActivity {
 
         SharedPreferences.Editor editor = sharedPrefs.edit();
 
-        editor.putString(actualListName, sharedPrefString + actualItem + ", ");
+        editor.putString("grocery list", sharedPrefString + actualItem + ", ");
 
         editor.apply();
 
-        sharedPrefString = sharedPrefs.getString(actualListName, "Empty List");
+        sharedPrefString = sharedPrefs.getString("grocery list", "Empty List");
 
-        finish();
-        startActivity(getIntent());
+        arrayAdapter.notifyDataSetChanged();
+
+        //finish();
+        //startActivity(getIntent());
     }
 }
