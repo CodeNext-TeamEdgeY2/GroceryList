c6191d172ba052b04dd57735cafe791c61ee9090
David Ung <davidu@csedge.org>
2020-06-26 22:15:38 -0700

    work
    POG CHAMP

---
+++ app/src/main/java/com/example/grocerylist/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.grocerylist;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 1500);
+
+    }
+
+}
